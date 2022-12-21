455abda42cc04764b4a16fd2168fea9a15097efb
DzenanDemirovic <ddemirovicdzenan5@gmail.com>
2022-12-21 23:20:59 +0100

    First commit

---
+++ Java-Script/8.cas/app.js
@@ -0,0 +1,62 @@
+// word="ABE";
+// isIsogram=true;
+// if(word.length=== 0){
+//     console.log("Na string je iz0gram");
+//     return;
+// }
+// for(i=0;i<word.length;i++){
+// for(j-i+1;j<word.length;j++){
+//     if(word[i]===word[j]){
+//         isIsogram=false;
+//         break;
+//     }
+// }
+// }
+// Dat je niz i granicna verdnost.Odrediti granicni nizi da li je jednak ili ispod niza
+// niz=[10,333,890,4,13,100];
+// granicnaVrednost=100;
+// ispodGRanice=true;
+// for(i=0;i<niz.length;i++){
+//     if(niz[i]>granicnaVrednost){ 
+//     console.log("Iznad granicne vrednosti")
+//     ispodGranice=false;
+//     break;
+// }
+// }
+// console.log(ispodGranice)
+// ako rec ima manje od 10 slova da se ispisa u potpunosti a ako vise ip=spisati prvi poslednji i broj slova
+// rec="lokalizacija";
+// if(rec.length<10){
+//     console.log(rec);
+// }
+// else{
+//     console.log(rec[0], rec[rec.length-1],rec.length - 2);
+// }
+// niz=[10,-11,25,-4,2,-22,22];
+// nasNiz=[ ]
+// brojac=0;
+// sumaNegativnih=0;
+
+//     if(niz.lenght===0){
+//         return [];
+//     }
+// for(i=0;i<niz.lenght;i++){ 
+// if(niz[i]<0){
+//     brojac++;
+
+// }else{
+//     sumaNegativnih+=niz[i];
+// }
+// }
+// nasNiz.push(brojac);
+// nasNiz.push(sumaNegativnih);
+// console.log(nasNiz);
+prvi="Elektrotehnika";
+drugi="Racunarstvo"
+
+if(prvi.lenght<drugi.lenght){
+    console.log(prvi,drugi,prvi);
+
+}else{
+    console.log(drugi,prvi,drugi);
+}
---
+++ Java-Script/9.cas/app.js
@@ -0,0 +1,76 @@
+// rec=("Dzenan ima auto");
+// brojSlova=0;
+// for(i=0;i<rec.length;i++){
+//     if(rec[i].toLowerCase()==="a")
+//     brojSlova++;
+    
+// }
+// // console.log(brojSlova);
+// UNesi string i vidi koliko ima malih slova
+// rec=prompt("Unesi neku rec");
+// brojacSlova=0;
+// for(i=0;i<rec.length;i++){
+//     if(rec[i]===rec[i].toLowerCase() && rec[i]!== (" ") ){ 
+//         brojacSlova++;
+//     }
+// }
+// console.log(brojacSlova);
+// Ispisato da li je u unotemu stringu ima vise slova malij ili velikih
+// rec="Afirmacija";
+// malaSlova=0;
+// velikaSlova=0;
+// for(i=0;i<rec.length;i++){
+//     if(rec[i]===rec[i].toLowerCase() && rec[i]!== " "){
+//         malaSlova++;
+//     }  else if(rec[i]===rec[i].toUperCase()&&rec[i]!==" ")
+//     velikaSlova++;
+// }
+// if(malaSlova>velikaSlova){
+//     console.log("IMa vise mali slova");
+// }
+// else if(malaSlova<velikaSlova){
+//     console.log("IMa vise slova");
+// }
+// else{
+//     console.log("IMa jednako slova");
+// } 
+// Izdvojiti jedinstveni niz ,elementu bez ponavljanja
+//  const niz=[10,11,33,4,555,2,1];
+// noviNiz=[];
+//  let jedinstveni=false;
+// for(let i=0;i<niz.length;i++){
+//     jedinstveni=false;
+// for(let j=0;j<noviNiz.length;i++){
+//     if(niz[i]===noviNiz[j]){
+//         jedinstveni=true;
+//         break;
+//     }
+// }
+// if(jedinstveni){
+//     continue;
+// }
+// else{
+//     noviNiz.push(niz[i]);
+// }
+// }
+//  let prvaRec="Mis";
+//  let drugaRec="Kompjuter";
+// let novaRec=" ";
+// let lenght=0;
+// if(prvaRec.length>drugaRec.length){
+//     lenght=prvaRec.length;
+// }
+// else{
+//     length=drugaRec.length;
+// }
+// for( let i=0;i<lenght;i++){
+//     if(prvaRec[i]=== undefined){
+//         novaRec+=drugaRec
+//         continue;
+//     }
+//     if(drugaRec[i]===undefined){
+//         novaRec+=prvaRec[i];
+//     }
+//     novaRec=prvaRec [i] +drugaRec[i];
+// }
+// console.log(novaRec);
