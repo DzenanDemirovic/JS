d83c7cbc53fdc25f1b0332ac91169440ae58c9c8
DzenanDemirovic <ddemirovicdzenan5@gmail.com>
2022-12-24 23:21:12 +0100

    First commit

---
+++ 10.cas/app.js
@@ -0,0 +1,52 @@
+// skraceno za if sa uslovima 
+//  // uslov ? true : false;
+// // str=n%2===0 ?"paran" : "neparan";
+// // console.log(str);
+// n=165;
+// n=n%10<5 ?( n+=2);(n-=5)
+// console.log
+// Naci prestupnu godinu ako je deljiv sa 4 ali nije sa 100
+// Broj godine je deljiv sa 100 i sa 400
+//  let start=1000;
+// let end=2000;
+// for(let i=start;i<end;i++ ){
+//     if(i%4===0 && i % 100 !==0){
+//         console.log("Godina je prestupna")
+//     }else if(i%100 %% i % 400 ===0){
+//         console.log("godina  nije prrstupna,i")
+//     }
+
+//     }else{
+//         continue;
+//     }
+        
+//     }
+// }
+//
+//  let niz=[4,3,5,8,9,0]
+
+// for(let i=0;i<niz.length;i++){
+//     for(let j=i+1;niz.length,i++){
+//         if(niz[i]<niz[j]){
+//             let pom = niz[i]
+//             niz[i]=niz[j]
+//             niz[j=pom]
+    
+//         }
+
+//     }
+// }console.log(niz)
+//Iz stringa 'pera ima devojku' i N=6 dobija se "Pera i"
+// ako ima vise ispisati ceo
+// let str="Fanta Orange";
+// let n=6;
+// noviStr= " "
+// for(i=0;i<n;i++){
+//     if(str.length<n){
+//         console.log(str);
+//         break;
+//     }
+// noviStr+=str[i]
+    
+// }
+// console.log(noviStr)
---
+++ 11.cas/app1.js
@@ -0,0 +1,2 @@
+console.log("Hello World");
+Prosti tipovi podataka
