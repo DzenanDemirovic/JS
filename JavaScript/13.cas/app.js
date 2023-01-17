0927aae1a4f3354a030dbf46abfde8c1533862fd
DzenanDemirovic <ddemirovicdzenan5@gmail.com>
2023-01-17 20:28:19 +0100

    first commit

---
+++ JavaScript/13.cas/app.js
@@ -0,0 +1,48 @@
+// var n=prompt("Unesi neki broj");
+// if(typeof n==="string"){
+//     console.log("Greska");
+// }else{ 
+//     console.log(n**2); 
+// }
+// recenica="Nakon danasnjeg testa cemo znati ko ce nastaviti sa drugim ciklusom";
+// novaRecenica=""
+
+// for(i=0;i<rec.length;i++){  
+//     if(rec[i]==="a" || rec[i]==="e" ||rec[i]==="i"|| rec[i]==="o" ||rec[i]==="u"){
+//         novaRecenica+=recenica[i].toUpperCas9=();
+//     }else{
+//         novaRecenica+=recenica[i];
+//     }
+// }
+//     console.log(novaRecenica + " . ");
+// latice=["i love you","a little","a lot","passionately","madly","not at all"];
+// let brojLatica=10;
+// console.log(latice[(brojLatica % latice.legth-1 )  ]);
+// const number=[1,2,3,4,5,6];
+// let rezultat=0
+// for(i=0;i<number.length;i++){
+// rezultat+=number[i]**2
+// }
+// console.log(rezultat);
+const like =["Ensar","Mehmed","Dzenis","Dzenan","Dzejlana"]
+switch(like.length){
+case 0:
+console.log("No one likes you");
+break;
+
+case 1 :
+    console.log(like[0]+"likes u ")
+    break;
+
+    case 2
+
+
+
+
+
+
+
+
+
+
+}
