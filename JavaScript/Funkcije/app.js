dc21cf307d88127837342052727dfd98ee4946cc
DzenanDemirovic <ddemirovicdzenan5@gmail.com>
2023-01-21 15:36:55 +0100

    first commit

---
+++ JavaScript/Funkcije/app.js
@@ -0,0 +1,53 @@
+//function NazivFunkcije(){
+    //console.log("Ispisi nesto")
+//}
+// NazivFunkcije();
+//
+// function saberi(firstNumber,secondNumber){
+// console.log (firstNumber+secondNumber)
+// }
+// console.log(10,15)
+// saberi(10,15)
+// function niz(arr){
+//     for(i=0;i< arr.length;i++){
+//         console.log(arr.[i]);
+
+//     }
+
+// }
+// const array = [1,3,4,6,7]
+// niz(array)
+//da se kvadriraju svi brojevi
+
+// const array = [10,22,1,3,5,6]
+//  const newArray=array.map((x)=>x**2)
+//  console.log(newArray)
+// const array=[2,4,5,6,18,19];
+// console.log(array.filter(x=>x%2===0));
+// console.log(array.filter(x=>x>10))
+// array=[10,15,45,10,65,78,98,45,15]
+// uniqueArray=[]
+// function uniqueArray(array){
+//     for(let i = 0;i<array.length;i++){
+//         if(uniqueArray.includes(array[i])){
+//             continue;
+//         }
+//         uniqueArray.push(array[i]);
+//     }
+//     return uniqueArray
+// }
+// console.log(uniqueArray(array));
+
+// newStr= " "
+// function uniqueInOrder(str){
+// for(i=0;i<str.lenght;i++){
+//     if(str[i]===str[i+1]){
+// continue;
+//     }
+//     newStr.push(str[i])
+// }
+// return newStr
+// }
+// str=("AAABBCCDAABB")
+// console.log(uniqueInOrder(str));
+//Domaci:
