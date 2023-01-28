35247063d704d0a75abd9767374d948668c938b2
DzenanDemirovic <ddemirovicdzenan5@gmail.com>
2023-01-28 18:39:15 +0100

    first commit

---
+++ JavaScript/Vezbanje/app.js
@@ -0,0 +1,23 @@
+// const osoba ={
+// ime = "Dzenan",
+// prezime="Demirovic"
+// //imeIprezime: function (){
+// //return this.ime + this.prezime
+// //
+
+// //}
+// age : 23;
+// punoletan : function(){
+//     if(this.age>=18){
+//         return "Punoletan"
+//     }
+//     else{
+//         return "Maloletan"
+//     }
+//    }
+// }
+// console.log(osoba.imeIprezime());
+// console.log(osoba.punoletan());
+// console.log(osoba.prezime);
+// console.log(osoba["ime"]);
+let
---
+++ JavaScript/While petlja/app.js
@@ -0,0 +1,64 @@
+// while(uslov)
+// kod
+// let i =10;
+// while(n>0){
+//     console.log(n)
+//     n--;
+// }
+// let i = 1;
+// while (i<10){
+//     console.log(i);
+//     i++;
+// }
+// n=10;
+// do{
+//     console.log(n)
+//     n++;
+
+// }while(n<10);
+
+// str = "abcde"
+// function provera (str){
+//     if(str.length%2===0){
+//         return (str.slice(0,str.length /2)+ "|"+  str.slice(str.length / 2 ,str.length));
+//     }
+//     else{
+//    return (str.slice(0,str.length /2)+ "|"+ str.slice(str.length/2 +1,str.length));
+//     }
+
+
+
+// }
+// console.log(provera("abcde "));
+
+
+//  str="My name is John"
+// function crta(str,sep){
+//    return str.split("").join('-');
+//  }
+// console.log(crta("My name is John"));
+// Uraditi faktorijel od nekog broja
+// let n = 6
+// proizvod=1;
+// while(n>0){
+//     proizvod *=n;
+//     n--;
+
+// }
+// console.log(proizvod);
+//Sabrati sumu parnih brojeva//
+// let n = 20
+// zbir=0;
+// while(n>0){
+// if(n%2===0){
+//     zbir+=n
+// }
+// }
+// console.log(zbir);
+// let niz=[1,2,3,4,5,6;]
+// niz.reduce((prethodna,trenutna) => prethodna + trenutna, " ");
+// prethodna =0, trenutna 1= 1, 0+1 = 1
+// prethodna 1, trenutna = 2 , 1+2=3
+// Objekat name = "dog",legs:4,color="white"
+
+
