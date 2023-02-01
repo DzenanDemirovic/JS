d5f10cbc51088c8bd126971e1c7368939d05447c
DzenanDemirovic <ddemirovicdzenan5@gmail.com>
2023-02-01 18:05:53 +0100

    first commit

---
+++ JavaScript/Vezbanje/app1.js
@@ -0,0 +1,17 @@
+//Napisato funkciju koja vraca string u kojem je ime zamenjeno prezime
+// let imeIprezime = "Nejla Fijuljanin"
+// function reverse(str){
+//     return imeIprezime.split(" ").reverse().join(" ")
+// }
+
+// console.log(reverse(imeIprezime));
+//const input = " Every developer likes to mix kubernetes and javascript" 
+//koristiti  split mapp i join
+// za rec uzeti prvo slovo i poslednje slovo i prebrojati slova izmedji
+// reci koje imaju manje od 4 slova ne skracuju.
+ 
+// const sentence = " Every developer likes to mix kubernetes and javascript" 
+// function newWord (str){
+//     return  sentence.split("").map((x)=>x.length>4 ? x[0]+ (x.length-2)+x(el.length -1) :x).join
+// }
+// console.log(sentence)
