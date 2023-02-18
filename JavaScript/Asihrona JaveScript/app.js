a3b29e8b4eca62350fc1d3c0e72d4610edba5dbf
DzenanDemirovic <ddemirovicdzenan5@gmail.com>
2023-02-18 15:42:24 +0100

    commit

--- JavaScript/Asihrona JaveScript/app.js
+++ JavaScript/Asihrona JaveScript/app.js
@@ -32,7 +32,101 @@
 // const rez = ParniElemeniti(Provera,array);
 
 
-
+function reduce(array, callback, initialValue) {
+    if(typeof initalValue==='undefined'){
+      initialValue===array[0];
+      array.shift();
+    }
+    
+    function Checking(array1,array2){
+    newArray = [];
+      for (let i =0;i<array.length;i++;){
+        if(array1.includes(array2[2])){
+    newArrray.push(array2[i]);
+       return newArray;
+        }
+        
+        
+      }
+    }
+    // Challenge 7
+    function intersection(arrays) {
+    return reduce(arrays,Checking,);
+    }
+    
+    
+       } console.log(intersection([[5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]]));
+     should log: [5, 15]
+     / Type JavaScript here and click "Run Code" or press Ctrl + s
+     console.log('Hello, world!');
+     
+     
+     // Challenge 1
+     function addTwo(num) {
+       return num+2;
+     
+     }
+     
+     // To check if you've completed it, uncomment these console.logs!
+     // console.log(addTwo(3));
+     // console.log(addTwo(10));
+     
+     
+     // Challenge 2
+     function addS(word) {
+     return word+"s";
+     }
+     
+     // uncomment these to check your work
+     // console.log(addS('pizza'));
+     // console.log(addS('bagel'));
+     
+     
+     // Challenge 3
+     function map(array, callback) {
+      const newArray=[];
+     for(let i=0;i<array.length;i++){
+     newArray.push(callback(array[i]));
+     }
+       return newArray;
+     }
+      console.log(map([1, 2, 3], addTwo));
+     
+     
+     // Challenge 4
+     function forEach(array, callback) {
+     for(let i=0;i<array.length;i++){
+       callback (array[i]);
+       
+     }
+     
+     }
+     
+     // see for yourself if your forEach works!
+     
+     
+     // Challenge 5
+     function mapWith(array, callback) {
+     const newArray=[];
+     forEach(array,function(num)){
+       newArray.push(callback(num));
+     }
+     return newArray;
+     }
+     
+     
+     // Challenge 6
+     function reduce(array, callback, initialValue) {
+         for(let i =0;i<array.length;i++){ 
+           value initialValue=callback(initialValue,array[i]);
+          } return initialValue
+          
+        
+        
+        }
+        console.log(reduce([1,2,3])addTwo 0);
+     }
+         
 
 
 
