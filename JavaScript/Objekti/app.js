f5e94d1b187d449db7e5042d4f5d365957e3ee4d
DzenanDemirovic <ddemirovicdzenan5@gmail.com>
2023-03-14 17:52:38 +0100

    objekti

--- JavaScript/Objekti/app.js
+++ JavaScript/Objekti/app.js
@@ -26,49 +26,49 @@
 // 4. smanjenjeBrzine: (metoda koja uzima jedan argument),
 // 5. koci: (metoda koja dodeljuje 0 za vrednost trenutne brzine).
  
-const myCar = {
-    id: 1,
-    marka: "Audi",
-    model: "a4",
-    boja: "Crvena",
-    pogon: "prednji",
-    menjac: "automatski",
-    kontakt: [0622222, 02033322],
-    servis: {
-      datum: "04,maj",
-      km: 23000,
-      serviser: "Pasovic",
-    },
-    udaran: true,
-  };
+// const myCar = {
+//     id: 1,
+//     marka: "Audi",
+//     model: "a4",
+//     boja: "Crvena",
+//     pogon: "prednji",
+//     menjac: "automatski",
+//     kontakt: [0622222, 02033322],
+//     servis: {
+//       datum: "04,maj",
+//       km: 23000,
+//       serviser: "Pasovic",
+//     },
+//     udaran: true,
+//   };
    
-//    1.
-    myCar.trenutnaBrzina = 100;
-    myCar.maksimalnaBrzina = 260;
-    console.log(myCar);
-    myCar.povecanjeBrzine= function (ubrznje){
-        if(this.trenutnaBrzina+ubrznje > this.maksimalnaBrzina){
-            return " nije moguce povecati brzinu datog ubrzanja";
-        }
-        else { 
-        this.trenutnaBrzina+=ubrznje;
-    }
+// //    1.
+//     myCar.trenutnaBrzina = 100;
+//     myCar.maksimalnaBrzina = 260;
+//     console.log(myCar);
+//     myCar.povecanjeBrzine= function (ubrznje){
+//         if(this.trenutnaBrzina+ubrznje > this.maksimalnaBrzina){
+//             return " nije moguce povecati brzinu datog ubrzanja";
+// //         }
+// //         else { 
+//         this.trenutnaBrzina+=ubrznje;
+//     }
 
-    }
-    myCar.smanjenjeBrzine = function(brzina){
-        if(this.trenutnaBrzina-brzina<0){
-           return  "Nije moguce voziti manje od 0 km/h"
-        }else {
-            this.trenutnaBrzina-=brzina;
-        }
-
-
-        myCar.kocenje = function (){
-this.trenutnaBrzina=0;
-
-        }
+//     }
+//     myCar.smanjenjeBrzine = function(brzina){
+//         if(this.trenutnaBrzina-brzina<0){
+//            return  "Nije moguce voziti manje od 0 km/h"
+//         }else {
+//             this.trenutnaBrzina-=brzina;
+//         }
 
 
-    }
+//         myCar.kocenje = function (){
+// this.trenutnaBrzina=0;
 
-    console.log(myCar);
+//         }
+
+
+//     }
+
+//     console.log(myCar);
