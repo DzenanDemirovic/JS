c315969da937001a55060b709d25a904ea8adbb4
DzenanDemirovic <ddemirovicdzenan5@gmail.com>
2023-03-22 01:54:00 +0100

    User Data

---
+++ JavaScript/Users Data/app.js
@@ -0,0 +1,35 @@
+ let glavni = document.querySelector(".container");
+ 
+ async function getAllUsers(){
+let data = await fetch("https://jsonplaceholder.typicode.com/users");
+let response = data.json();
+return response
+  }
+  let podaciKorisnika ;
+  getAllUsers().then((d) => {
+    let podaciKorisnika = d;
+
+podaciKorisnika.forEach(element => {
+    createCard(element.name,element.username,element.id);
+});
+
+    console.log(podaciKorisnika)
+  });
+
+function createCard(name,username,id){
+let card = document.createElement("div");
+card.className = "card"
+let ime = document.createElement("p");
+ime.innerText= "Ime"+ " " + name;
+let userNAme = document.createElement("username")
+userNAme.innerText = "username" + " " + username
+
+card.addEventListener("click", ()=>{
+    window.location.href = `index1.html?id=${id}`;
+});
+
+card.appendChild(ime);
+card.append(userNAme);
+glavni.appendChild(card);
+
+}
---
+++ JavaScript/Users Data/app1.js
@@ -0,0 +1,9 @@
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+console.log(id);
+async function getAllUsers(){
+    let data = await fetch(`https://jsonplaceholder.typicode.com/users/${id}/post`
+    );
+    let response = data.json();
+    return response
+      }
